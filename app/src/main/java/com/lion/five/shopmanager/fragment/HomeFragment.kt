e1903b16d20cb5bf1acb497456a95a28498f135f
StopStone <wltjr6432@naver.com>
2024-11-24 16:03:41 +0900

    feat: 메뉴, 버튼 리스너 추가 - 검색 버튼 클릭 시 검색 화면 이동 - 상품 추가 버튼 클릭 시 삼품 추가 화면으로 이동 - 화면 이동 함수 확장함수 분리

--- app/src/main/java/com/lion/five/shopmanager/fragment/HomeFragment.kt
+++ app/src/main/java/com/lion/five/shopmanager/fragment/HomeFragment.kt
@@ -5,7 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.lion.five.shopmanager.R
 import com.lion.five.shopmanager.databinding.FragmentHomeBinding
+import com.lion.five.shopmanager.utils.replaceFragment
 
 class HomeFragment: Fragment() {
     private var _binding: FragmentHomeBinding? = null
@@ -22,10 +24,31 @@ class HomeFragment: Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setListeners()
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    /*
+    * 리스너들 모음
+    * */
+    private fun setListeners() {
+        with(binding) {
+            // 메뉴 클릭시
+            toolbarHome.setOnMenuItemClickListener {
+                when(it.itemId) {
+                    R.id.navigation_search -> replaceFragment(SearchFragment(), "SearchFragment")
+                }
+                true
+            }
+
+            // 상품 추가 버튼을 눌렀을 때
+            btnProductAddAction.setOnClickListener {
+                replaceFragment(AddProductFragment(), "AddProductFragment")
+            }
+        }
     }
 }
