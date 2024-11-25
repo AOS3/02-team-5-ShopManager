ca7f6efb8bc941f8e73e7f2f06f2524b866af198
StopStone <wltjr6432@naver.com>
2024-11-26 00:15:01 +0900

    fix: 카테고리명 캐치티니핑 > 티니핑으로 변경

---
+++ app/src/main/java/com/lion/five/shopmanager/data/model/ProductCategory.kt
@@ -0,0 +1,10 @@
+package com.lion.five.shopmanager.data.model
+
+enum class ProductCategory(val categoryName: String) {
+    DISNEY("디즈니"),
+    PIXAR("픽사"),
+    MARVEL("마블"),
+    HARRY_POTTER("해리포터"),
+    GHIBLI("지브리"),
+    TEENIEPING("티니핑")
+}
