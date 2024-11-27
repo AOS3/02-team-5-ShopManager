6f5a7c0e051e3c07ae87f9a830f968d3f0a361b8
StopStone <wltjr6432@naver.com>
2024-11-27 18:11:42 +0900

    refactor: database null check

--- app/src/main/java/com/lion/five/shopmanager/data/dao/ProductDatabase.kt
+++ app/src/main/java/com/lion/five/shopmanager/data/dao/ProductDatabase.kt
@@ -8,25 +8,21 @@ import androidx.room.TypeConverters
 import com.lion.five.shopmanager.data.converter.Converters
 import com.lion.five.shopmanager.data.vo.ProductVO
 
-@Database(entities = [ProductVO::class], version = 1, exportSchema = true)
+@Database(entities = [ProductVO::class], version = 1)
 @TypeConverters(Converters::class)
-abstract class ProductDatabase : RoomDatabase(){
-    abstract fun productDAO() : ProductDAO
+abstract class ProductDatabase : RoomDatabase() {
+    abstract fun productDAO(): ProductDAO
 
-    companion object{
-        var productDatabase:ProductDatabase? = null
-        @Synchronized
-        fun getInstance(context: Context) : ProductDatabase?{
-            synchronized(ProductDatabase::class){
-                productDatabase = Room.databaseBuilder(
-                    context.applicationContext, ProductDatabase::class.java,
+    companion object {
+        private var instance: ProductDatabase? = null
+
+        fun getInstance(context: Context) =
+            instance ?: synchronized(this) {
+                instance ?: Room.databaseBuilder(
+                    context,
+                    ProductDatabase::class.java,
                     "Product.db"
                 ).build()
             }
-            return productDatabase
-        }
-        fun destroyInstance(){
-            productDatabase = null
-        }
     }
 }
