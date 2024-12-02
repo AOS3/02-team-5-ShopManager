e6f21d632c77842f95c7fd6507b2bf798f0a45f8
StopStone <wltjr6432@naver.com>
2024-12-02 19:33:28 +0900

    feat: 유효성 검증 및 확장함수를 활용한 예외처리

--- app/src/main/java/com/lion/five/shopmanager/utils/Extensions.kt
+++ app/src/main/java/com/lion/five/shopmanager/utils/Extensions.kt
@@ -3,6 +3,8 @@ package com.lion.five.shopmanager.utils
 import android.content.Context
 import android.view.View
 import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
+import androidx.core.content.edit
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.commit
 import com.lion.five.shopmanager.R
@@ -32,4 +34,34 @@ fun View.clearFocusAndHideKeyboard() {
 fun Context.hideKeyboard(view: View) {
     val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
     imm.hideSoftInputFromWindow(view.windowToken, 0)
+}
+
+fun Context.isLogin() =
+    getSharedPreferences("user_prefs", Context.MODE_PRIVATE)
+        .getBoolean("is_logged_in", false)
+
+
+fun Context.getAccount(): Pair<String, String> =
+    getSharedPreferences("user_prefs", Context.MODE_PRIVATE).run {
+        val id = getString("account_id", "") ?: ""
+        val password = getString("account_password", "") ?: ""
+        id to password
+    }
+
+fun Context.setupLogin(isLogin: Boolean) {
+    getSharedPreferences("user_prefs", Context.MODE_PRIVATE)
+        .edit {
+            putBoolean("is_logged_in", isLogin)
+        }
+    }
+
+fun Context.clearAccount() {
+    getSharedPreferences("user_prefs", Context.MODE_PRIVATE)
+        .edit {
+            clear()
+        }
+}
+
+fun Context.showMessage(message: String) {
+    Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
 }
