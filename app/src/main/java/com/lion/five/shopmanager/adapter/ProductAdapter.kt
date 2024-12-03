0f5be26bc2eac5c15d8d32b858b6b1a84dc86cd0
StopStone <wltjr6432@naver.com>
2024-12-03 23:38:32 +0900

    fix: 상품의 이미지가 다른 상품의 이미지로 변환되는 현상 수정 - imageView의 tag를 이용

--- app/src/main/java/com/lion/five/shopmanager/adapter/ProductAdapter.kt
+++ app/src/main/java/com/lion/five/shopmanager/adapter/ProductAdapter.kt
@@ -1,6 +1,5 @@
 package com.lion.five.shopmanager.adapter
 
-import android.graphics.BitmapFactory
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.core.view.isVisible
@@ -11,7 +10,12 @@ import com.lion.five.shopmanager.data.model.Product
 import com.lion.five.shopmanager.databinding.ItemProductBinding
 import com.lion.five.shopmanager.listener.OnItemClickListener
 import com.lion.five.shopmanager.utils.FileUtil
+import com.lion.five.shopmanager.utils.loadImage
 import com.lion.five.shopmanager.utils.toDecimalFormat
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class ProductAdapter(
     private val listener: OnItemClickListener,
@@ -44,22 +48,24 @@ class ProductAdapter(
         fun bind(product: Product) {
             with(binding) {
                 tvProductTitle.text = product.name
-                itemView.post {
-                    Thread {
-                        val imageFile = FileUtil.loadImageFile(itemView.context, product.images.first())
-                        val options = BitmapFactory.Options().apply { inSampleSize = 4 }
-                        val bitmap = BitmapFactory.decodeFile(imageFile.absolutePath, options)
-
-                        itemView.post {
-                            ivProductThumbnail.setImageBitmap(bitmap)
-                        }
-                    }.start()
-                }
-
                 tvProductIsBest.isVisible = product.isBest
                 tvProductPrice.text = "${product.price.toDecimalFormat()}원"
                 tvProductReviewCount.text = if (product.reviewCount == 0) "리뷰 없음" else "${product.reviewCount}"
                 tvProductType.text = product.type
+
+                // 이미지 로딩 전에 태그 설정
+                ivProductThumbnail.tag = product.images.first()
+                CoroutineScope(Dispatchers.IO).launch {
+                    val bitmap = FileUtil.loadImageFile(itemView.context, product.images.first())
+                        .loadImage()
+
+                    withContext(Dispatchers.Main) {
+                        // 비동기 이미지 로딩 중 뷰가 재사용된 경우를 체크
+                        if (ivProductThumbnail.tag == product.images.first()) {
+                            ivProductThumbnail.setImageBitmap(bitmap)
+                        }
+                    }
+                }
             }
         }
     }
