0869b826f2f3fde0725827ee204584eab5384bbf
StopStone <wltjr6432@naver.com>
2024-12-03 23:45:08 +0900

    이미지 리스트 분리

--- app/src/main/java/com/lion/five/shopmanager/data/Storage.kt
+++ app/src/main/java/com/lion/five/shopmanager/data/Storage.kt
@@ -1,5 +1,6 @@
 package com.lion.five.shopmanager.data
 
+import com.lion.five.shopmanager.R
 import com.lion.five.shopmanager.data.model.Product
 
 object Storage {
@@ -328,5 +329,36 @@ object Storage {
             stock = 0,
             reviewCount = 0,
         )
+    )
+
+    val movieImages = listOf(
+        // 지브리
+        R.drawable.ghibli_1, R.drawable.ghibli_2, R.drawable.ghibli_3, R.drawable.ghibli_4,
+        R.drawable.ghibli_5, R.drawable.ghibli_6, R.drawable.ghibli_7, R.drawable.ghibli_8,
+        R.drawable.ghibli_9, R.drawable.ghibli_10, R.drawable.ghibli_11, R.drawable.ghibli_12,
+
+        // 마블
+        R.drawable.marvel_1, R.drawable.marvel_2, R.drawable.marvel_3, R.drawable.marvel_4,
+        R.drawable.marvel_5, R.drawable.marvel_6, R.drawable.marvel_7, R.drawable.marvel_8,
+        R.drawable.marvel_9, R.drawable.marvel_10, R.drawable.marvel_11,
+
+        // 픽사
+        R.drawable.pixar_1, R.drawable.pixar_2, R.drawable.pixar_3, R.drawable.pixar_4,
+        R.drawable.pixar_5, R.drawable.pixar_6, R.drawable.pixar_7, R.drawable.pixar_8,
+        R.drawable.pixar_9,
+
+        // 디즈니
+        R.drawable.disney_1, R.drawable.disney_2, R.drawable.disney_3, R.drawable.disney_4,
+        R.drawable.disney_5, R.drawable.disney_6, R.drawable.disney_7, R.drawable.disney_8,
+        R.drawable.disney_9, R.drawable.disney_10,
+
+        // 해리포터
+        R.drawable.harrypotter_1, R.drawable.harrypotter_2, R.drawable.harrypotter_3, R.drawable.harrypotter_4,
+        R.drawable.harrypotter_5, R.drawable.harrypotter_6, R.drawable.harrypotter_7, R.drawable.harrypotter_8,
+        R.drawable.harrypotter_9, R.drawable.harrypotter_10,
+
+        // 티니핑
+        R.drawable.teenieping_1, R.drawable.teenieping_2, R.drawable.teenieping_3, R.drawable.teenieping_4,
+        R.drawable.teenieping_5
     )
 }
